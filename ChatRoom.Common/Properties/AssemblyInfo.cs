453f7e6ffb7a6ab778a789cc1158406107771bfe
nbclw <1027477807@qq.com>
2018-04-22 21:25:30 +0800

    服务端修改

--- ChatRoom.Common/Properties/AssemblyInfo.cs
+++ ChatRoom.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ChatRoomCommon")]
+[assembly: AssemblyTitle("ChatRoom.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChatRoomCommon")]
+[assembly: AssemblyProduct("ChatRoom.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("3fd25db8-0fa8-4519-88c2-76e324d1593a")]
+[assembly: Guid("9e2fd797-4726-4a0e-8844-862922bc3fcf")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
