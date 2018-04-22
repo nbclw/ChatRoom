661908f7337ae84ed72213e6d3f3bee309a1e1cc
nbclw <1027477807@qq.com>
2018-04-22 21:57:52 +0800

    1

--- ChatRoom.Common/Class1.cs
+++ ChatRoom.Common/Class1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatRoom.Common
 {
-    public class Class1
+    public class TcpModel
     {
+        private TcpClient _tcp;
+
+        public TcpModel(string ip, int port)
+        {
+            _tcp = new TcpClient(ip, port);
+        }
     }
 }
